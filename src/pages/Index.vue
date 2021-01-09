dbd3070b75ee8ba35eae46f6849b5e30f14f81a5
kurikinton105 <yamadakenta0101@gmail.com>
2021-01-09 23:44:57 +0900

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -94,45 +94,6 @@
           </div>
         </q-timeline-entry>
 
-        <q-timeline-entry
-          title="写真あげMASKぁ〜？？"
-          subtitle="September 1-12, 2020"
-          side="left"
-          icon ="camera"
-        >
-          <div>
-            良い写真撮れたからSNSにあげたい！でも、知らない人の顔ガッツリ写り込んでるし、使いづらい、、。そんなお悩み解決しつつ、手軽に「写真あげMASKぁ～？？」。※煽っているわけではありません。<br>
-            <a href="https://icy-moss-046b41900.azurestaticapps.net/">  https://icy-moss-046b41900.azurestaticapps.net/</a><br>
-            (<a href="https://hacku.yahoo.co.jp/hacku2020online3/">  Open Hack U 2020 vol.3 最優秀作品 </a>)
-          </div>
-        </q-timeline-entry>
-
-        <q-timeline-entry
-          title="StudyOurSelves (SOS)"
-          subtitle="August 15-16, 2020"
-          side="right"
-          icon="today"
-        >
-          <div>
-            ToDoタスク共有＋クイズ共有＝みんなで勉強<br>
-            皆さんは、テスト前に友達と問題出し合うことをしたことはありますか？ありますよね。しかしオンライン授業によって問題を出し合うのが難しい。さらに宿題の量も増えてToDoがたくさん...しかし、大丈夫。StudyOurSelvesがあればみんなでToDoとクイズが共有できます。<br>
-            #サマーハッカソン・<a href="https://talent.supporterz.jp/geekten/2020/">技育展</a>参加作品<br>
-            <a href="http://studyourselves.azurewebsites.net/">  http://studyourselves.azurewebsites.net/</a>
-          </div>
-        </q-timeline-entry>
-
-        <q-timeline-entry
-          title="Make Class Easy"
-          subtitle="July 22, 2020"
-          side="left"
-          icon="library_books"
-        >
-          <div>
-            「Make Class Easy」は授業の理解を補助するサービスである。授業中に教授が口頭で話した内容を文字に起こし、要約した文章を生成する。また、要約した文章およびその他の任意の文章にて専門用語が出てきた場合、用語の意味について注釈を加え、数式が出てきた際には何の公式や理論を用いているのかを補足した新たな文章を生成する。これらの機能を用いて学習効率を向上できる。<br>
-            「機械学習」最終プロジェクトのプロトタイプです。<br>
-            <a href="https://summary-classes-web.azurewebsites.net/">  https://summary-classes-web.azurewebsites.net/</a>
-          </div>
-        </q-timeline-entry>
       </q-timeline>
       </div>
     </q-page>
@@ -168,67 +129,6 @@
           </q-item>
           <br>
         </q-card>
-        </div>&nbsp;&nbsp;
-        <div class="col-auto">
-          <q-card class="my-card" flat bordered>
-            <br>
-            <q-item>
-              <q-item-section avatar>
-                <q-avatar>
-                  <img src="https://gyazo.com/d9f84923f103dded87fb6769db010e70/thumb/1000">
-                </q-avatar>
-              </q-item-section>
-
-              <q-item-section>
-                <q-item-label><a href= "https://qiita.com/y_a_m_a/items/2fef42212f5ffeb11531" >Azure Web AppsではじめるFlask入門</a></q-item-label>
-                <q-item-label caption>
-                  2020年12月25-2日
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-            <br>
-          </q-card>
-        </div>&nbsp;&nbsp;
-        <div class="col-auto">
-          <q-card class="my-card" flat bordered>
-            <br>
-            <q-item>
-              <q-item-section avatar>
-                <q-avatar>
-                  <img src="~assets/qiita-qiita-jobs-favicon.png">
-                </q-avatar>
-              </q-item-section>
-
-              <q-item-section>
-                <q-item-label><a href= "https://qiita.com/y_a_m_a/items/2fef42212f5ffeb11531" >Azure Web AppsではじめるFlask入門</a></q-item-label>
-                <q-item-label caption>
-                  2020年12月25-3日
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-            <br>
-          </q-card>
-
-          <div class="col-auto">
-          <q-card class="my-card" flat bordered>
-            <br>
-            <q-item>
-              <q-item-section avatar>
-                <q-avatar>
-                  <img src="~assets/qiita-qiita-jobs-favicon.png">
-                </q-avatar>
-              </q-item-section>
-
-              <q-item-section>
-                <q-item-label><a href= "https://qiita.com/y_a_m_a/items/2fef42212f5ffeb11531" >Azure Web AppsではじめるFlask入門</a></q-item-label>
-                <q-item-label caption>
-                  2020年12月25-4日
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-            <br>
-          </q-card>
-        </div>
 
         </div>
       </div>
